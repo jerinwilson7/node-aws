31047538f76cf87867a3d234f5cc93056865201d
JerinWilson <jerin.wilson@vonnue.com>
2025-01-30 17:16:43 +0530

    feat: add dotenv

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,8 @@ import express, { Application, Request, Response } from "express";
 
 const app:Application = express();
 
-const port = process.env.PORT
 dotenv.config()
+const port = process.env.PORT
 
 app.get("/", (req:Request, res:Response) => {
   res.send("HI THIS IS FROM THE SERVER");
